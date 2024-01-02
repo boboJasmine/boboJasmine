8fec0b0324f447b25f357bcd299064864b18fab7
boboJasmine <148369040+boboJasmine@users.noreply.github.com>
2024-01-02 10:09:00 +0800

    Update AOI_script.js

--- scripts/AOI_script.js
+++ scripts/AOI_script.js
@@ -2,401 +2,74 @@ const changeImage = document.getElementById("sliderButton");
 const image1 = document.getElementById("foregroundImg");
 const image2 = document.getElementById("backgroundImg");
 
-let imageNames1 = ["cate2-00001.jpg",
-    "cate2-00007.jpg",
-    "cate2-00008.jpg",
-    "cate2-00010.jpg",
-    "cate2-00011.jpg",
-    "cate2-00012.jpg",
-    "cate2-00013.jpg",
-    "cate2-00015.jpg",
-    "cate2-00016.jpg",
-    "cate2-00017.jpg",
-    "cate2-00018.jpg",
-    "cate2-00019.jpg",
-    "cate2-00020.jpg",
-    "cate2-00021.jpg",
-    "cate2-00024.jpg",
-    "cate2-00026.jpg",
-    "cate2-00027.jpg",
-    "cate2-00028.jpg",
-    "cate2-00029.jpg",
-    "cate2-00030.jpg",
-    "cate2-00031.jpg",
-    "cate2-00032.jpg",
-    "cate2-00033.jpg",
-    "cate2-00034.jpg",
-    "cate2-00036.jpg",
-    "cate2-00037.jpg",
-    "cate2-00038.jpg",
-    "cate2-00039.jpg",
-    "cate2-00040.jpg",
-    "cate2-00101.jpg",
-    "cate2-00105.jpg",
-    "cate2-00107.jpg",
-    "cate2-00108.jpg",
-    "cate2-00111.jpg",
-    "cate2-00112.jpg",
-    "cate2-00113.jpg",
-    "cate2-00114.jpg",
-    "cate2-00115.jpg",
-    "cate2-00116.jpg",
-    "cate2-00118.jpg",
-    "cate2-00120.jpg",
-    "cate2-00122.jpg",
-    "cate2-00123.jpg",
-    "cate2-00124.jpg",
-    "cate2-00126.jpg",
-    "cate2-00127.jpg",
-    "cate2-00128.jpg",
-    "cate2-00129.jpg",
-    "cate2-00130.jpg",
-    "cate2-00133.jpg",
-    "cate2-00134.jpg",
-    "cate2-00135.jpg",
-    "cate2-00136.jpg",
-    "cate2-00138.jpg",
-    "cate2-00139.jpg",
-    "cate2-00140.jpg",
-    "cate2-00142.jpg",
-    "cate2-00143.jpg",
-    "cate2-00145.jpg",
-    "cate2-00146.jpg",
-    "cate2-00147.jpg",
-    "cate2-00150.jpg",
-    "cate2-00151.jpg",
-    "cate2-00152.jpg",
-    "cate2-00153.jpg",
-    "cate2-00154.jpg",
-    "cate2-00157.jpg",
-    "cate2-00159.jpg",
-    "cate2-00160.jpg",
-    "cate2-00162.jpg",
-    "cate2-00163.jpg",
-    "cate2-00165.jpg",
-    "cate2-00167.jpg",
-    "cate2-00170.jpg",
-    "cate2-00171.jpg",
-    "cate2-00172.jpg",
-    "cate2-00173.jpg",
-    "cate2-00174.jpg",
-    "cate2-00175.jpg",
-    "cate2-00176.jpg",
-    "cate2-00177.jpg",
-    "cate2-00178.jpg",
-    "cate2-00179.jpg",
-    "cate2-00180.jpg",
-    "cate2-00182.jpg",
-    "cate2-00183.jpg",
-    "cate2-00185.jpg",
-    "cate2-00186.jpg",
-    "cate2-00187.jpg",
-    "cate2-00188.jpg",
-    "cate2-00189.jpg",
-    "cate2-00191.jpg",
-    "cate2-00192.jpg",
-    "cate2-00193.jpg",
-    "cate2-00194.jpg",
-    "cate2-00197.jpg",
-    "cate2-00198.jpg",
-    "cate2-00199.jpg",
-    "cate2-00200.jpg",
-    "cate2-00201.jpg",
-    "cate2-00202.jpg",
-    "cate2-00203.jpg",
-    "cate2-00204.jpg",
-    "cate2-00205.jpg",
-    "cate2-00206.jpg",
-    "cate2-00207.jpg",
-    "cate2-00209.jpg",
-    "cate2-00211.jpg",
-    "cate2-00212.jpg",
-    "cate2-00214.jpg",
-    "cate2-00215.jpg",
-    "cate2-00216.jpg",
-    "cate2-00217.jpg",
-    "cate2-00218.jpg",
-    "cate2-00219.jpg",
-    "cate2-00220.jpg",
-    "cate3-00003.jpg",
-    "cate3-00006.jpg",
-    "cate3-00007.jpg",
-    "cate3-00008.jpg",
-    "cate3-00009.jpg",
-    "cate3-00013.jpg",
-    "cate3-00014.jpg",
-    "cate3-00016.jpg",
-    "cate3-00018.jpg",
-    "cate3-00019.jpg",
-    "cate3-00020.jpg",
-    "cate3-00021.jpg",
-    "cate3-00023.jpg",
-    "cate3-00026.jpg",
-    "cate3-00027.jpg",
-    "cate3-00028.jpg",
-    "cate3-00029.jpg",
-    "cate3-00031.jpg",
-    "cate3-00032.jpg",
-    "cate3-00033.jpg",
-    "cate3-00034.jpg",
-    "cate3-00035.jpg",
-    "cate3-00036.jpg",
-    "cate3-00037.jpg",
-    "cate3-00039.jpg",
-    "cate3-00040.jpg",
-    "cate3-00041.jpg",
-    "cate3-00042.jpg",
-    "cate3-00043.jpg",
-    "cate3-00044.jpg",
-    "cate3-00045.jpg",
-    "cate4-00069.jpg",
-    "cate4-00070.jpg",
-    "cate4-00072.jpg",
-    "cate4-00073.jpg",
-    "cate4-00075.jpg",
-    "cate4-00077.jpg",
-    "cate4-00079.jpg",
-    "cate4-00082.jpg",
-    "cate4-00085.jpg",
-    "cate4-00086.jpg",
-    "cate4-00087.jpg",
-    "cate4-00088.jpg",
-    "cate4-00089.jpg",
-    "cate4-00091.jpg",
-    "cate4-00094.jpg",
-    "cate4-00095.jpg",
-    "cate4-00099.jpg",
-    "cate4-00100.jpg",
-    "cate4-00101.jpg",
-    "cate4-00102.jpg",
-    "cate4-00103.jpg",
-    "cate4-00104.jpg",
-    "cate4-00105.jpg",
-    "cate4-00106.jpg",
-    "cate4-00107.jpg",
-    "cate4-00108.jpg",
-    "cate4-00111.jpg",
-    "cate4-00112.jpg",
-    "cate4-00113.jpg",
-    "cate4-00114.jpg",
-    "cate4-00116.jpg",
-    "cate4-00117.jpg",
-    "cate4-00118.jpg",
-    "cate4-00119.jpg",
-    "cate4-00120.jpg",
-    "cate4-00121.jpg",
-    "cate4-00124.jpg",
-    "cate4-00127.jpg",
-    "cate4-00129.jpg",
-    "cate4-00130.jpg",
-    "cate4-00131.jpg",
-    "cate4-00133.jpg",
-    "cate4-00134.jpg",
-    "cate4-00135.jpg",
-    "cate4-00137.jpg",
-    "cate4-00139.jpg"]; 
+let imageNames1 = ["exp1_num_3667.jpg", "exp1_num_4727.jpg", "exp1_num_4944.jpg", "exp1_num_6984.jpg",
+    "exp1_num_9473.jpg", "exp1_num_36290.jpg", "exp1_num_40410.jpg", "exp1_num_51674.jpg",
+    "exp1_num_54246.jpg", "exp1_num_108719.jpg", "exp1_num_108889.jpg", "exp1_num_262480.jpg",
+    "exp1_num_265077.jpg", "exp1_num_290998.jpg", "exp1_num_297464.jpg", "exp1_num_308015.jpg",
+    "exp1_num_317483.jpg", "exp1_num_322605.jpg", "exp1_num_327970.jpg", "exp1_num_346311.jpg",
+    "exp2_num_3696.jpg", "exp2_num_4748.jpg", "exp2_num_4964.jpg", "exp2_num_7009.jpg",
+    "exp2_num_9496.jpg", "exp2_num_36302.jpg", "exp2_num_40424.jpg", "exp2_num_51697.jpg",
+    "exp2_num_54273.jpg", "exp2_num_108741.jpg", "exp2_num_108923.jpg", "exp2_num_262572.jpg",
+    "exp2_num_265103.jpg", "exp2_num_291023.jpg", "exp2_num_297490.jpg", "exp2_num_308043.jpg",
+    "exp2_num_317512.jpg", "exp2_num_322630.jpg", "exp2_num_327997.jpg", "exp3_num_853.jpg",
+    "exp3_num_3710.jpg", "exp3_num_4764.jpg", "exp3_num_4978.jpg", "exp3_num_7023.jpg",
+    "exp3_num_9508.jpg", "exp3_num_36246.jpg", "exp3_num_40438.jpg", "exp3_num_51711.jpg",
+    "exp3_num_54291.jpg", "exp3_num_108757.jpg", "exp3_num_262601.jpg", "exp3_num_265117.jpg",
+    "exp3_num_291038.jpg", "exp3_num_297506.jpg", "exp3_num_308061.jpg", "exp3_num_317526.jpg",
+    "exp3_num_322643.jpg", "exp3_num_328014.jpg", "exp3_num_346347.jpg", "exp4_num_3724.jpg",
+    "exp4_num_4779.jpg", "exp4_num_4995.jpg", "exp4_num_7036.jpg", "exp4_num_9520.jpg",
+    "exp4_num_36258.jpg", "exp4_num_40450.jpg", "exp4_num_51725.jpg", "exp4_num_54308.jpg",
+    "exp4_num_108774.jpg", "exp4_num_262626.jpg", "exp4_num_265131.jpg", "exp4_num_291052.jpg",
+    "exp4_num_297524.jpg", "exp4_num_308078.jpg", "exp4_num_317540.jpg", "exp4_num_322656.jpg",
+    "exp4_num_328029.jpg", "exp5_num_3746.jpg", "exp5_num_4801.jpg", "exp5_num_5022.jpg",
+    "exp5_num_7054.jpg", "exp5_num_9549.jpg", "exp5_num_36269.jpg", "exp5_num_40480.jpg",
+    "exp5_num_51739.jpg", "exp5_num_54331.jpg", "exp5_num_108791.jpg", "exp5_num_262654.jpg",
+    "exp5_num_265149.jpg", "exp5_num_291077.jpg", "exp5_num_297552.jpg", "exp5_num_308098.jpg",
+    "exp5_num_317558.jpg", "exp5_num_322672.jpg", "exp5_num_328057.jpg", "exp5_num_346381.jpg",
+    "exp6_num_3789.jpg", "exp6_num_4841.jpg", "exp6_num_5106.jpg", "exp6_num_7102.jpg",
+    "exp6_num_9594.jpg", "exp6_num_36280.jpg", "exp6_num_40515.jpg", "exp6_num_51771.jpg",
+    "exp6_num_54366.jpg", "exp6_num_108812.jpg", "exp6_num_262705.jpg", "exp6_num_265182.jpg",
+    "exp6_num_291134.jpg", "exp6_num_297601.jpg", "exp6_num_308130.jpg", "exp6_num_317592.jpg",
+    "exp6_num_322697.jpg", "exp6_num_328105.jpg", "exp6_num_346414.jpg"];
 
-let imageNames2 = ["cate2-00001.bmp",
-    "cate2-00007.bmp",
-    "cate2-00008.bmp",
-    "cate2-00010.bmp",
-    "cate2-00011.bmp",
-    "cate2-00012.bmp",
-    "cate2-00013.bmp",
-    "cate2-00015.bmp",
-    "cate2-00016.bmp",
-    "cate2-00017.bmp",
-    "cate2-00018.bmp",
-    "cate2-00019.bmp",
-    "cate2-00020.bmp",
-    "cate2-00021.bmp",
-    "cate2-00024.bmp",
-    "cate2-00026.bmp",
-    "cate2-00027.bmp",
-    "cate2-00028.bmp",
-    "cate2-00029.bmp",
-    "cate2-00030.bmp",
-    "cate2-00031.bmp",
-    "cate2-00032.bmp",
-    "cate2-00033.bmp",
-    "cate2-00034.bmp",
-    "cate2-00036.bmp",
-    "cate2-00037.bmp",
-    "cate2-00038.bmp",
-    "cate2-00039.bmp",
-    "cate2-00040.bmp",
-    "cate2-00101.bmp",
-    "cate2-00105.bmp",
-    "cate2-00107.bmp",
-    "cate2-00108.bmp",
-    "cate2-00111.bmp",
-    "cate2-00112.bmp",
-    "cate2-00113.bmp",
-    "cate2-00114.bmp",
-    "cate2-00115.bmp",
-    "cate2-00116.bmp",
-    "cate2-00118.bmp",
-    "cate2-00120.bmp",
-    "cate2-00122.bmp",
-    "cate2-00123.bmp",
-    "cate2-00124.bmp",
-    "cate2-00126.bmp",
-    "cate2-00127.bmp",
-    "cate2-00128.bmp",
-    "cate2-00129.bmp",
-    "cate2-00130.bmp",
-    "cate2-00133.bmp",
-    "cate2-00134.bmp",
-    "cate2-00135.bmp",
-    "cate2-00136.bmp",
-    "cate2-00138.bmp",
-    "cate2-00139.bmp",
-    "cate2-00140.bmp",
-    "cate2-00142.bmp",
-    "cate2-00143.bmp",
-    "cate2-00145.bmp",
-    "cate2-00146.bmp",
-    "cate2-00147.bmp",
-    "cate2-00150.bmp",
-    "cate2-00151.bmp",
-    "cate2-00152.bmp",
-    "cate2-00153.bmp",
-    "cate2-00154.bmp",
-    "cate2-00157.bmp",
-    "cate2-00159.bmp",
-    "cate2-00160.bmp",
-    "cate2-00162.bmp",
-    "cate2-00163.bmp",
-    "cate2-00165.bmp",
-    "cate2-00167.bmp",
-    "cate2-00170.bmp",
-    "cate2-00171.bmp",
-    "cate2-00172.bmp",
-    "cate2-00173.bmp",
-    "cate2-00174.bmp",
-    "cate2-00175.bmp",
-    "cate2-00176.bmp",
-    "cate2-00177.bmp",
-    "cate2-00178.bmp",
-    "cate2-00179.bmp",
-    "cate2-00180.bmp",
-    "cate2-00182.bmp",
-    "cate2-00183.bmp",
-    "cate2-00185.bmp",
-    "cate2-00186.bmp",
-    "cate2-00187.bmp",
-    "cate2-00188.bmp",
-    "cate2-00189.bmp",
-    "cate2-00191.bmp",
-    "cate2-00192.bmp",
-    "cate2-00193.bmp",
-    "cate2-00194.bmp",
-    "cate2-00197.bmp",
-    "cate2-00198.bmp",
-    "cate2-00199.bmp",
-    "cate2-00200.bmp",
-    "cate2-00201.bmp",
-    "cate2-00202.bmp",
-    "cate2-00203.bmp",
-    "cate2-00204.bmp",
-    "cate2-00205.bmp",
-    "cate2-00206.bmp",
-    "cate2-00207.bmp",
-    "cate2-00209.bmp",
-    "cate2-00211.bmp",
-    "cate2-00212.bmp",
-    "cate2-00214.bmp",
-    "cate2-00215.bmp",
-    "cate2-00216.bmp",
-    "cate2-00217.bmp",
-    "cate2-00218.bmp",
-    "cate2-00219.bmp",
-    "cate2-00220.bmp",
-    "cate3-00003.bmp",
-    "cate3-00006.bmp",
-    "cate3-00007.bmp",
-    "cate3-00008.bmp",
-    "cate3-00009.bmp",
-    "cate3-00013.bmp",
-    "cate3-00014.bmp",
-    "cate3-00016.bmp",
-    "cate3-00018.bmp",
-    "cate3-00019.bmp",
-    "cate3-00020.bmp",
-    "cate3-00021.bmp",
-    "cate3-00023.bmp",
-    "cate3-00026.bmp",
-    "cate3-00027.bmp",
-    "cate3-00028.bmp",
-    "cate3-00029.bmp",
-    "cate3-00031.bmp",
-    "cate3-00032.bmp",
-    "cate3-00033.bmp",
-    "cate3-00034.bmp",
-    "cate3-00035.bmp",
-    "cate3-00036.bmp",
-    "cate3-00037.bmp",
-    "cate3-00039.bmp",
-    "cate3-00040.bmp",
-    "cate3-00041.bmp",
-    "cate3-00042.bmp",
-    "cate3-00043.bmp",
-    "cate3-00044.bmp",
-    "cate3-00045.bmp",
-    "cate4-00069.bmp",
-    "cate4-00070.bmp",
-    "cate4-00072.bmp",
-    "cate4-00073.bmp",
-    "cate4-00075.bmp",
-    "cate4-00077.bmp",
-    "cate4-00079.bmp",
-    "cate4-00082.bmp",
-    "cate4-00085.bmp",
-    "cate4-00086.bmp",
-    "cate4-00087.bmp",
-    "cate4-00088.bmp",
-    "cate4-00089.bmp",
-    "cate4-00091.bmp",
-    "cate4-00094.bmp",
-    "cate4-00095.bmp",
-    "cate4-00099.bmp",
-    "cate4-00100.bmp",
-    "cate4-00101.bmp",
-    "cate4-00102.bmp",
-    "cate4-00103.bmp",
-    "cate4-00104.bmp",
-    "cate4-00105.bmp",
-    "cate4-00106.bmp",
-    "cate4-00107.bmp",
-    "cate4-00108.bmp",
-    "cate4-00111.bmp",
-    "cate4-00112.bmp",
-    "cate4-00113.bmp",
-    "cate4-00114.bmp",
-    "cate4-00116.bmp",
-    "cate4-00117.bmp",
-    "cate4-00118.bmp",
-    "cate4-00119.bmp",
-    "cate4-00120.bmp",
-    "cate4-00121.bmp",
-    "cate4-00124.bmp",
-    "cate4-00127.bmp",
-    "cate4-00129.bmp",
-    "cate4-00130.bmp",
-    "cate4-00131.bmp",
-    "cate4-00133.bmp",
-    "cate4-00134.bmp",
-    "cate4-00135.bmp",
-    "cate4-00137.bmp",
-    "cate4-00139.bmp"];  //接收圖片陣列
+let imageNames2 = ["exp1_num_3667.png", "exp1_num_4727.png", "exp1_num_4944.png", "exp1_num_6984.png",
+    "exp1_num_9473.png", "exp1_num_36290.png", "exp1_num_40410.png", "exp1_num_51674.png",
+    "exp1_num_54246.png", "exp1_num_108719.png", "exp1_num_108889.png", "exp1_num_262480.png",
+    "exp1_num_265077.png", "exp1_num_290998.png", "exp1_num_297464.png", "exp1_num_308015.png",
+    "exp1_num_317483.png", "exp1_num_322605.png", "exp1_num_327970.png", "exp1_num_346311.png",
+    "exp2_num_3696.png", "exp2_num_4748.png", "exp2_num_4964.png", "exp2_num_7009.png",
+    "exp2_num_9496.png", "exp2_num_36302.png", "exp2_num_40424.png", "exp2_num_51697.png",
+    "exp2_num_54273.png", "exp2_num_108741.png", "exp2_num_108923.png", "exp2_num_262572.png",
+    "exp2_num_265103.png", "exp2_num_291023.png", "exp2_num_297490.png", "exp2_num_308043.png",
+    "exp2_num_317512.png", "exp2_num_322630.png", "exp2_num_327997.png", "exp3_num_853.png",
+    "exp3_num_3710.png", "exp3_num_4764.png", "exp3_num_4978.png", "exp3_num_7023.png",
+    "exp3_num_9508.png", "exp3_num_36246.png", "exp3_num_40438.png", "xp3_num_51711.png",
+    "exp3_num_54291.png", "exp3_num_108757.png", "exp3_num_262601.png", "exp3_num_265117.png",
+    "exp3_num_291038.png", "exp3_num_297506.png", "exp3_num_308061.png", "exp3_num_317526.png",
+    "exp3_num_322643.png", "exp3_num_328014.png", "exp3_num_346347.png", "exp4_num_3724.png",
+    "exp4_num_4779.png", "exp4_num_4995.png", "exp4_num_7036.png", "exp4_num_9520.png",
+    "exp4_num_36258.png", "exp4_num_40450.png", "exp4_num_51725.png", "exp4_num_54308.png",
+    "exp4_num_108774.png", "exp4_num_262626.png", "exp4_num_265131.png", "exp4_num_291052.png",
+    "exp4_num_297524.png", "exp4_num_308078.png", "exp4_num_317540.png", "exp4_num_322656.png",
+    "exp4_num_328029.png", "exp5_num_3746.png", "exp5_num_4801.png", "exp5_num_5022.png",
+    "exp5_num_7054.png", "exp5_num_9549.png", "exp5_num_36269.png", "exp5_num_40480.png",
+    "exp5_num_51739.png", "exp5_num_54331.png", "exp5_num_108791.png", "exp5_num_262654.png",
+    "exp5_num_265149.png", "exp5_num_291077.png", "exp5_num_297552.png", "exp5_num_308098.png",
+    "exp5_num_317558.png", "exp5_num_322672.png", "exp5_num_328057.png", "exp5_num_346381.png",
+    "exp6_num_3789.png", "exp6_num_4841.png", "exp6_num_5106.png", "exp6_num_7102.png",
+    "exp6_num_9594.png", "exp6_num_36280.png", "exp6_num_40515.png", "exp6_num_51771.png",
+    "exp6_num_54366.png", "exp6_num_108812.png", "exp6_num_262705.png", "exp6_num_265182.png",
+    "exp6_num_291134.png", "exp6_num_297601.png", "exp6_num_308130.png", "exp6_num_317592.png",
+    "exp6_num_322697.png", "exp6_num_328105.png", "exp6_num_346414.png"];  //接收圖片陣列
 
 let currentIndex1 = 0;
 let currentIndex2 = 0;
-let oldIndex1 = -1;
+// let oldIndex1 = -1;
 
 // 圖片相對路徑
-const folder1 = "./牙齒/train/images/"; 
-const folder2 = "./牙齒/train/masks/";
+const folder1 = "./瑕疵完成/img/";
+const folder2 = "./瑕疵完成/mask/";
+let newImageName1, newImageName2, newImageUrl1, newImageUrl2;
 
 let beginingAnimation; // 先顯示的圖
 const imageAnimation = document.getElementById("run");
@@ -404,21 +77,21 @@ const dotWaveAnimation = document.querySelector(".change_animation");
 // const delayInMilliseconds = 3000;
 const next = document.getElementById("nextButton");
 
-imageAnimation.addEventListener("click", () => {    
+imageAnimation.addEventListener("click", () => {
     if (currentIndex1 = currentIndex2) {
         beginingAnimation = true;
     }
 
     if (currentIndex1 = 0) {
-        const newImageName1 = imageNames1[currentIndex1];
-        const newImageName2 = imageNames2[currentIndex2];
-    
-        const newImageUrl1 = folder1 + newImageName1;
-        const newImageUrl2 = folder2 + newImageName2;
-    
+        newImageName1 = imageNames1[currentIndex1];
+        newImageName2 = imageNames2[currentIndex2];
+
+        newImageUrl1 = folder1 + newImageName1;
+        newImageUrl2 = folder2 + newImageName2;
+
         image1.src = newImageUrl1;
         image1.alt = "Image " + (currentIndex1 + 1);
-    
+
         image2.src = newImageUrl2;
         image2.alt = "Image " + (currentIndex2 + 1);
     }
@@ -447,8 +120,8 @@ changeImage.addEventListener("click", () => {
     image2.style.display = "none";
     image1.style.display = "flex";/* 比賽時新增的 */
     image1.style.opacity = "1"; /* 比賽時新增的 */
-    
-    
+
+
     if ((currentIndex1 < 0)) {
         currentIndex1 = 0;
         currentIndex2 = 0;
@@ -459,16 +132,19 @@ next.addEventListener("click", () => {
     dotWaveAnimation.style.display = "none";
     image2.style.display = "none";
     image1.style.display = "flex";/* 比賽時新增的 */
+    // image1.style.visibility = "visible";
     image1.style.opacity = "1"; /* 比賽時新增的 */
-
+    
     currentIndex1 = (currentIndex1 + 1) % imageNames1.length;
+    console.log(currentIndex1);
     currentIndex2 = (currentIndex2 + 1) % imageNames2.length;
+    console.log(currentIndex1, currentIndex2);
 
-    const newImageName1 = imageNames1[currentIndex1];
-    const newImageName2 = imageNames2[currentIndex2];
+    newImageName1 = imageNames1[currentIndex1];
+    newImageName2 = imageNames2[currentIndex2];
 
-    const newImageUrl1 = folder1 + newImageName1;
-    const newImageUrl2 = folder2 + newImageName2;
+    newImageUrl1 = folder1 + newImageName1;
+    newImageUrl2 = folder2 + newImageName2;
 
     image1.src = newImageUrl1;
     image1.alt = "Image " + (currentIndex1 + 1);
@@ -515,7 +191,7 @@ next.addEventListener("click", () => {
 //     image2.style.display = "none";
 //     image1.style.display = "flex";/* 比賽時新增的 */
 //     image1.style.opacity = "1"; /* 比賽時新增的 */
-    
+
 //     if ((currentIndex1 - oldIndex1) < 1) {
 //         oldIndex1 = (oldIndex1 - 2) % imageNames1.length;
 //         currentIndex1 = (currentIndex1 - 1) % imageNames1.length;
@@ -531,7 +207,7 @@ next.addEventListener("click", () => {
 //         }
 
 //     } 
-    
+
 //     if ((currentIndex1 >= 0) && (oldIndex1 >= -1)) {
 //         currentIndex1 = (currentIndex1 - 1) % imageNames1.length;
 //         currentIndex2 = (currentIndex2 - 1) % imageNames2.length;
@@ -543,7 +219,7 @@ next.addEventListener("click", () => {
 //         }
 
 //     } 
-    
+
 //     if ((currentIndex1 < 0) || (oldIndex1 < -1)) {
 //         currentIndex1 = 0;
 //         currentIndex2 = 0;
@@ -608,7 +284,7 @@ next.addEventListener("click", () => {
 
 //     image2.src = newImageUrl2;
 //     image2.alt = "Image " + (currentIndex2 + 1);
-    
+
 // })
 
 // ---------------------file catch------------------------------
@@ -684,3 +360,5 @@ window.addEventListener('scroll', () => {
         navbar.style.backgroundColor = '#ffffff00';
     }
 });
+
+//------------------------------失敗---------------------------------
